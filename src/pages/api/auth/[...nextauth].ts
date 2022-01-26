ed879ea2138375e1b3dcbf3e08fdd1a07e5d2884
Silvio Romano Neto <silvioromanoneto@gmail.com>
2022-01-26 08:12:42 -0300

    feat: Implementing payment methods with Stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,13 +24,18 @@ export default NextAuth({
       try {
         await fauna.query(
           q.If(
-            q.Not(q.Exists(q.Match("user_by_email"), q.Casefold(email))),
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
             q.Create(q.Collection("users"), { data: { email } }),
-            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
         );
+
         return true;
-      } catch {
+      } catch (err) {
         return false;
       }
     },
